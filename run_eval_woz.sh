998150eaa2a44c2422c4b89e97e50f939de3b8d4
chin0880ee <test123@gmail.com>
2022-12-27 02:15:56 +0800

    name

--- run_eval_woz.sh
+++ run_eval_woz.sh
@@ -6,6 +6,6 @@ pythonpath='python3 -m'
 # set dir
 data_name='multiwoz'     # ['kvr', 'multiwoz']
 data_dir=./data/MULTIWOZ2.1
-eval_dir=./outputs/MULTIWOZ2.1/state_epoch_8.model
+eval_dir=./outputs/MULTIWOZ2.1/state_epoch_7.model
 
 ${pythonpath} tools.eval --data_name=${data_name} --data_dir=${data_dir} --eval_dir=${eval_dir}
