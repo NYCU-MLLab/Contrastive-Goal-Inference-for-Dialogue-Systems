1c7ad1a4e74e20c6564b5b0d58a35fd65765a760
chin0880ee <87538234+chin0880ee@users.noreply.github.com>
2023-01-05 15:51:59 +0800

    Update run_eval_smd.sh

--- run_eval_smd.sh
+++ run_eval_smd.sh
@@ -6,6 +6,6 @@ pythonpath='python3 -m'
 # set dir
 data_name='kvr'     # ['kvr', 'multiwoz']
 data_dir=./data/KVR
-eval_dir=./outputs/KVR/state_epoch_8.model
+eval_dir=./outputs/KVR/state_epoch_7.model
 
 ${pythonpath} tools.eval --data_name=${data_name} --data_dir=${data_dir} --eval_dir=${eval_dir}
